e706a134a4a271ebdfcb0c4e92537b1341d9d6d2
Zoe Meers <zoe.meers@gmail.com>
2018-05-11 15:43:04 +1000

    fix spelling

--- R/read_abs_sdmx.R
+++ R/read_abs_sdmx.R
@@ -1,4 +1,4 @@
-## Functions to read ABS SDXM URLs
+## Functions to read ABS SDMX URLs
 ## Zoe Meers
 ## The United States Studies Centre
 
@@ -17,6 +17,5 @@ read_abs_sdmx <- function(url) {
   url <- url
   dataset <- rsdmx::readSDMX(url)
   abs_data <- as.data.frame(dataset)
-
   return(abs_data)
 }
