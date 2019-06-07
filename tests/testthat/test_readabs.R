7945ffc6281c0e1e692ddb24e62c4bdcc9454ada
Matt Cowgill <mattcowgill@gmail.com>
2019-06-07 10:05:09 +1000

    ensure separate_series() tests don't run on CRAN

--- tests/testthat/test_readabs.R
+++ tests/testthat/test_readabs.R
@@ -284,6 +284,10 @@ test_that("read_cpi() returns appropriate errors",{
 
 test_that("separate_series() performs as expected",{
 
+ skip_on_cran()
+
+ check_abs_site()
+
  motor_vehicles <- read_abs("9314.0") %>% separate_series()
 
  expect_equal(14, length(motor_vehicles))
