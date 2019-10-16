1b85121d055596b7d7ab9fff88a4f27127463a6f
HughParsonage <hugh.parsonage@gmail.com>
2019-10-16 17:51:24 +1100

    Add coverage

--- tests/testthat/test-fst.R
+++ tests/testthat/test-fst.R
@@ -25,6 +25,9 @@ test_that("read_abs works out of the box", {
                  regexp = "but was not present in the local table and will be ignored",
                  fixed = TRUE)
 
+  expect_warning(read_abs(cat_no = "6401.0", tables = "not all"),
+                 regexp = "tables.*will be ignored")
+
 
 
 })
