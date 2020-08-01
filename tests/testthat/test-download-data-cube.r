cfdc69760988314a9b79fe89eaadf07f266ecfa5
David Diviny <david.diviny@gmail.com>
2020-08-02 06:50:33 +1000

    Change to tempdir

--- tests/testthat/test-download-data-cube.r
+++ tests/testthat/test-download-data-cube.r
@@ -1,29 +1,29 @@
-local_path <- "../testdata"
+temp_path <- tempdir()
 
 test_that("File is downloaded",{
   #Download May 2020 EQ09
-  local_path <- "../testdata"
+  temp_path <- tempdir()
 
   download_abs_data_cube(cat_no = "6291.0.55.003",
                          cube = "EQ09", date = "May 2020",
-                         path = local_path)
+                         path = temp_path)
 
-  expect_equal(file.exists(file.path(local_path, "eq09.zip")), TRUE)
+  expect_equal(file.exists(file.path(temp_path, "eq09.zip")), TRUE)
 })
 
 
 test_that("File can be unzipped",{
-  local_path <- "../testdata"
+  temp_path <- tempdir()
 
-  unzip(zipfile  = file.path(local_path, "eq09.zip"), files = "EQ09.xlsx", exdir = local_path)
-  expect_equal(file.exists(file.path(local_path, "eq09.xlsx")), TRUE)
+  unzip(zipfile  = file.path(temp_path, "eq09.zip"), files = "EQ09.xlsx", exdir = temp_path)
+  expect_equal(file.exists(file.path(temp_path, "eq09.xlsx")), TRUE)
 })
 
 
 test_that("File contents are as expected",{
-  local_path <- "../testdata"
+  temp_path <- tempdir()
 
-  eq09 <- readxl::read_excel(file.path(local_path, "EQ09.xlsx"), range = "A3",
+  eq09 <- readxl::read_excel(file.path(temp_path, "EQ09.xlsx"), range = "A3",
                      col_names = FALSE) %>%
     dplyr::pull()
   expect_equal(eq09, "Released at 11.30 am (Canberra time) 25 June 2020")
@@ -31,6 +31,6 @@ test_that("File contents are as expected",{
 
 
 #Delete downloaded and unzippedfile
-file.remove(file.path(local_path, "eq09.xlsx"))
-file.remove(file.path(local_path, "eq09.zip"))
+file.remove(file.path(temp_path, "eq09.xlsx"))
+file.remove(file.path(temp_path, "eq09.zip"))
 
