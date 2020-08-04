50bdea38ac99f2d4f1b7d1e8804d289c8d289e00
David Diviny <david.diviny@nousgroup.com.au>
2020-08-04 11:37:29 +1000

    Move temp_path out of tests.

--- tests/testthat/test-download-data-cube.r
+++ tests/testthat/test-download-data-cube.r
@@ -7,16 +7,12 @@ download_abs_data_cube(cat_no = "6291.0.55.003",
 
 test_that("File is downloaded",{
   #Download May 2020 EQ09
-  temp_path <- tempdir()
-
-
 
   expect_equal(file.exists(file.path(temp_path, "eq09.zip")), TRUE)
 })
 
 
 test_that("File can be unzipped",{
-  temp_path <- tempdir()
 
   unzip(zipfile  = file.path(temp_path, "eq09.zip"), files = "EQ09.xlsx", exdir = temp_path)
   expect_equal(file.exists(file.path(temp_path, "eq09.xlsx")), TRUE)
@@ -24,7 +20,6 @@ test_that("File can be unzipped",{
 
 
 test_that("File contents are as expected",{
-  temp_path <- tempdir()
 
   eq09 <- readxl::read_excel(file.path(temp_path, "EQ09.xlsx"), range = "A3",
                      col_names = FALSE) %>%
