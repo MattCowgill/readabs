3a0e06110f9d60d5893207854250b3d7483f30dc
David Diviny <david.diviny@nousgroup.com.au>
2020-08-04 12:24:45 +1000

    Move download back inside tests

--- tests/testthat/test-download-data-cube.r
+++ tests/testthat/test-download-data-cube.r
@@ -1,12 +1,13 @@
-temp_path <- tempdir()
-
-download_abs_data_cube(cat_no = "6291.0.55.003",
-                       cube = "EQ09", date = "May 2020",
-                       path = temp_path)
-
 
 test_that("File is downloaded",{
   #Download May 2020 EQ09
+
+  temp_path <- tempdir()
+
+  download_abs_data_cube(cat_no = "6291.0.55.003",
+                         cube = "EQ09", date = "May 2020",
+                         path = temp_path)
+
 
   expect_equal(file.exists(file.path(temp_path, "eq09.zip")), TRUE)
 })
@@ -14,12 +15,26 @@ test_that("File is downloaded",{
 
 test_that("File can be unzipped",{
 
+  temp_path <- tempdir()
+
+  download_abs_data_cube(cat_no = "6291.0.55.003",
+                         cube = "EQ09", date = "May 2020",
+                         path = temp_path)
+
   unzip(zipfile  = file.path(temp_path, "eq09.zip"), files = "EQ09.xlsx", exdir = temp_path)
   expect_equal(file.exists(file.path(temp_path, "eq09.xlsx")), TRUE)
 })
 
 
 test_that("File contents are as expected",{
+
+  temp_path <- tempdir()
+
+  download_abs_data_cube(cat_no = "6291.0.55.003",
+                         cube = "EQ09", date = "May 2020",
+                         path = temp_path)
+
+  unzip(zipfile  = file.path(temp_path, "eq09.zip"), files = "EQ09.xlsx", exdir = temp_path)
 
   eq09 <- readxl::read_excel(file.path(temp_path, "EQ09.xlsx"), range = "A3",
                      col_names = FALSE) %>%
