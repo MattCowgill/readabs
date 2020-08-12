703b698ae8e6b1f808c033fcbcd47b3348f6978f
Matt Cowgill <mattcowgill@gmail.com>
2020-08-12 10:09:03 +1000

    I hate Travis!

--- tests/testthat/test-download-data-cube.r
+++ tests/testthat/test-download-data-cube.r
@@ -35,35 +35,15 @@ test_that("File can be unzipped",{
                          cube = "EQ09", date = "May 2020",
                          path = temp_path)
 
-  unzip(zipfile  = file.path(temp_path, "eq09.zip"), files = "EQ09.xlsx", exdir = temp_path)
+  utils::unzip(zipfile  = file.path(temp_path, "eq09.zip"), files = "EQ09.xlsx", exdir = temp_path)
   expect_equal(file.exists(file.path(temp_path, "eq09.xlsx")), TRUE)
 
-  file.remove(file.path(temp_path, "eq09.zip"))
-  file.remove(file.path(temp_path, "eq09.xlsx"))
-})
-
-
-test_that("File contents are as expected",{
-
-  testthat::skip_on_cran()
-  check_abs_site()
-
-  temp_path <- tempdir()
-
-  download_abs_data_cube(cat_no = "6291.0.55.003",
-                         cube = "EQ09", date = "May 2020",
-                         path = temp_path)
-
-  unzip(zipfile  = file.path(temp_path, "eq09.zip"), files = "EQ09.xlsx", exdir = temp_path)
-
   eq09 <- readxl::read_excel(file.path(temp_path, "EQ09.xlsx"), range = "A3",
-                     col_names = FALSE) %>%
+                             col_names = FALSE) %>%
     dplyr::pull()
   expect_equal(eq09, "Released at 11.30 am (Canberra time) 25 June 2020")
 
   file.remove(file.path(temp_path, "eq09.zip"))
   file.remove(file.path(temp_path, "eq09.xlsx"))
 })
-
-
 
