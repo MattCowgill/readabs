292dafc1cb4fa2a0db120659a0355eb0ea08b9f9
David Diviny <david.diviny@nousgroup.com.au>
2020-09-23 13:16:46 +1000

    Put helper function in separate script.

--- R/show_available_catalogues.r
+++ R/show_available_catalogues.r
@@ -11,33 +11,4 @@ show_available_catalogues <- function(selected_heading = NULL){
 }
 
 
-get_available_cubes <- function(catalogue_string){
 
-
-  download_url <- filter(abs_lookup_table,
-                                  catalogue == catalogue_string) %>%
-    pull(url)
-
-  if(length(download_url) == 0) {stop(glue("No matching catalogue. Please check against ABS website."))}
-
-
-  #Try to download the page
-  download_page <- tryCatch(
-    xml2::read_html(download_url),
-    error=function(cond) {
-      message(paste("URL does not seem to exist:", download_url))
-      message("Here's the original error message:")
-      message(cond)
-      # Choose a return value in case of error
-      return(NA)}
-  )
-
-  #Find the url for the download
-  available_downloads <- tibble::tibble(label = download_page %>% rvest::html_nodes(".abs-data-download-left") %>% rvest::html_text(),
-                 url = download_page %>% rvest::html_nodes(".file a") %>%  rvest::html_attr("href")) %>%
-    mutate(file = str_extract(url, "[^/]*$")) %>%
-    select(label, file, url)
-
-  return(available_downloads)
-
-}
