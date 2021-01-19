f5e59918e2d94aca2b46fe44d07d2d437b7ebfee
Matt Cowgill <mattcowgill@gmail.com>
2021-01-19 15:16:44 +0800

    closes https://github.com/MattCowgill/readabs/issues/116

--- R/download_abs.R
+++ R/download_abs.R
@@ -1,4 +1,3 @@
-#' @importFrom utils download.file
 #' @importFrom purrr walk2
 
 download_abs <- function(urls,
@@ -8,33 +7,29 @@ download_abs <- function(urls,
   # create a subdirectory of the working directory with value given by
   # the path argument and define the filename that will be given to the
   # downloaded table
-  dir.create(path = path, recursive = TRUE, showWarnings = FALSE)
+  if (!dir.exists(path)) {
+    dir.create(path = path, recursive = TRUE, showWarnings = FALSE)
+  }
 
   filenames <- file.path(path, basename(urls))
 
-  # if libcurl is available we can vectorise urls and destfile to download
-  # files simultaneously; if not, we have to iterate
-  if (isTRUE(capabilities("libcurl"))) {
-    utils::download.file(
-      url = urls,
-      mode = "wb",
-      quiet = !show_progress_bars,
-      destfile = filenames,
-      method = "libcurl",
-      cacheOK = FALSE,
-      headers = c("User-Agent" = readabs_user_agent)
-    )
-  } else {
-    purrr::walk2(
-      .x = urls,
-      .y = filenames,
-      .f = utils::download.file,
-      mode = "wb",
-      quiet = !show_progress_bars,
-      cacheOK = FALSE,
-      headers = c("User-Agent" = readabs_user_agent)
+  dl_file <- function(url, filename, show_progress_bars) {
+    if (show_progress_bars) {
+      message("Downloading ", url)
+    }
+
+    curl::curl_download(
+      url = url,
+      destfile = filename,
+      quiet = !show_progress_bars
     )
   }
+
+  purrr::walk2(.x = urls,
+               .y = filenames,
+               .f = dl_file,
+               show_progress_bars = show_progress_bars)
+
 
   return(TRUE)
 }
