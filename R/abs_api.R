a3380432089e665e8f0e2464327cc74edbc74a36
kinto-b <kinto.behr@srcentre.com.au>
2022-06-08 10:01:43 +1000

    Accept datakey as named list

--- R/abs_api.R
+++ R/abs_api.R
@@ -1,3 +1,5 @@
+# Documentation: https://api.gov.au/assets/APIs/abs/DataAPI.openapi.html#/Get%20Data/GetData
+
 #' ABS API
 #'
 #' These functions provide a minimal interface to the ABS API.
@@ -5,16 +7,17 @@
 #'   - Using `abs_dataflows()` you can get information on the dataflows available
 #'   - Using `abs_datastructure()` you can get metadata relating to a specific dataflow
 #'   - Using `abs_data()` you can get the data belonging to a given dataflow.
-#'   Note that the API enforces a reasonably strict gateway timeout policy. This
-#'   means that, if you're trying to access a reasonably large dataset, you will
-#'   need to filter it on the server side using the `datakey`.
+#'
+#' Note that the API enforces a reasonably strict gateway timeout policy. This
+#' means that, if you're trying to access a reasonably large dataset, you will
+#' need to filter it on the server side using the `datakey`.
 #'
 #' More information can be found on the [ABS website](https://www.abs.gov.au/about/data-services/application-programming-interfaces-apis/data-api-user-guide/using-api)
 #'
 #' @param id A dataflow id, see `abs_dataflows()` for available dataflows.
-#' @param datakey The key to filter data on. This must be provided in
-#'   dot-delimited format. Position in the string is important. See
-#'   `abs_datastructure()` for information about dimension position and values.
+#' @param datakey A named list matching filter variables to codes. All variables
+#'   with a `position` in the datastructure are filterable. See
+#'   `abs_datastructure()` for information.
 #' @param start_period The start period (used to filter on time). This is
 #'   inclusive. The supported formats are:
 #'
@@ -56,7 +59,7 @@
 #' x[x$var=="SEX_ABS" & x$label == "Persons", ]
 #'
 #' # So we build a datakey putting 3 in the first and 0 in the fifth position to get filtered data
-#' y <- abs_data("ABS_C16_T10_SA", datakey = "3....0.", )
+#' y <- abs_data("ABS_C16_T10_SA", datakey = list(ASGS_2016 = 0, SEX= 3))
 #' unique(y["ASGS_2016"]) # Confirming only 'Australia' level records came through
 #' unique(y["SEX_ABS"]) # Confirming only 'Australia' level records came through
 #'
@@ -82,25 +85,41 @@ abs_dataflows <- function() {
 
 #' @export
 #' @rdname abs_api
-abs_data <- function(id, datakey = "all", start_period = NULL, end_period = NULL, version = NULL) {
-  # Build query
+abs_data <- function(id, datakey = NULL, start_period = NULL, end_period = NULL, version = NULL) {
+  # Fetch datastructure
+  datastructure <- abs_datastructure(id)
+  datastructure <- datastructure[!is.na(datastructure$code), ]
+
+  # Build data query
   dataflow <- paste0("ABS,", id)
   if (!is.null(version)) dataflow <- paste(dataflow, version, sep=",")
   q <- c("startPeriod" = start_period, "endPeriod" = end_period)
-  url <- abs_api_url(c("data", dataflow, datakey), q)
+  if (!is.null(datakey)) {
+    k <- abs_api_match_key(datakey, datastructure)
+  } else {
+    k <- "all"
+  }
+  url <- abs_api_url(c("data", dataflow, k), q)
 
-  # Query API for labels and data
-  codelist <- abs_datastructure(id)
-  codelist <- codelist[!is.na(codelist$code), ]
-
+  # Fetch data
   as_csv <- httr::accept("application/vnd.sdmx.data+csv")
   r <- httr::GET(url, as_csv)
+  if (httr::status_code(r) %in% 404 & !is.null(datakey)) {
+    url <- abs_api_url(c("data", dataflow, "all"), q)
+    r <- httr::GET(url, as_csv)
+    warning(call.=FALSE, paste(
+      "The query could not be executed with the provided `datakey`.",
+      "This is usually because one or more values provided do not appear in the data.",
+      "\n\n Retrying with `datakey=NULL`",
+      "Please check your data carefully!"
+    ))
+  }
   httr::stop_for_status(r)
   r <- resp_as_df(r)
 
   # Label values
   r <- purrr::imap_dfc(r, function(x, var_name) {
-    codes <- codelist[codelist$var == var_name, ]
+    codes <- datastructure[datastructure$var == var_name, ]
     if (nrow(codes)==0) return(x)
     labs <- codes$code
 
@@ -171,7 +190,7 @@ abs_datastructure <- function(id) {
 resp_as_df <- function(r) {
   fp <- tempfile()
   writeLines(rawToChar(httr::content(r)), fp)
-  r <- read.csv(fp)
+  r <- utils::read.csv(fp)
   unlink(fp)
 
   r
@@ -201,6 +220,67 @@ abs_api_url <- function(path, query = NULL) {
   out
 }
 
+
+#' Construct a datakey
+#'
+#' @param datakey A named list matching variables to codes
+#' @param datastructure A datastructure retrieved with `abs_datastructure()`
+#'
+#' @return A datakey string
+#' @noRd
+#' @keywords internal
+#'
+#' @examples
+#' abs_api_match_key(list(SEX=1:3, REGION="AUS"), z)
+#' abs_api_match_key(list(SEX=1:10, REGION="AUS"), z)
+#' try(abs_api_match_key(list(SX=1:3, REGION="AUS"), z))
+#' try(abs_api_match_key(list(UNIT_MEASURE="AUD", REGION="AUS"), z))
+abs_api_match_key <- function(datakey, datastructure) {
+  stopifnot("`datakey` must be a named list" = is.list(datakey))
+  stopifnot("`datakey` must be a named list" = length(names(datakey))==length(datakey))
+
+  bad_vars <- setdiff(names(datakey), datastructure$var)
+  if (length(bad_vars) > 0) {
+    stop(call. = FALSE,
+         "Variable(s) `", paste(bad_vars, collapse = "`, `"),
+         "` not found. Please review the datastructure with `abs_datastructure()`"
+    )
+  }
+
+  key <- purrr::imap_dfr(datakey, function(vals, var) {
+    ds <- datastructure[datastructure$var %in% var, ]
+
+    bad_vals <- setdiff(vals, ds$code)
+    if (length(bad_vals) > 0) {
+      if (length(bad_vals)>5) bad_vals <- c(bad_vals[1:5], "...")
+      warning(call. = FALSE,
+              "Variable `", var, "` does not contain codes: ",
+              paste(bad_vals, collapse=", ")
+      )
+    }
+
+    if (any(is.na(ds$position))) {
+      stop(call. = FALSE,
+           "Cannot filter on `", var,
+           "`. Please review the datastructure with `abs_datastructure()`"
+      )
+    }
+
+    ds[ds$code %in% vals, ]
+  })
+
+
+  pos <- unique(datastructure$position)
+  pos <- sort(pos[!is.na(pos)])
+
+  key <- purrr::map_chr(pos, function(pos) {
+    keyvals <- key[key$position == pos, ]
+    paste0(paste(keyvals$code, collapse = "+"), ".")
+  })
+
+  paste(key, collapse = "")
+}
+
 #' Can something be safely coerced to numeric?
 #'
 #' @param x A vector
@@ -208,8 +288,6 @@ abs_api_url <- function(path, query = NULL) {
 #' @return `TRUE` or `FALSE`
 #' @noRd
 #' @keywords internal
-#'
-#' @examples
 can_numeric <- function(x) {
   if (is.numeric(x) | is.integer(x) | is.logical(x) | is.factor(x)) {
     TRUE
