924e34ddfd1c86442a12e4b7a86249ca90075d41
“Matt <mattcowgill@gmail.com>
2022-08-16 14:43:07 +1000

    tweak payrolls test, as the ABS removes wages from some series

--- tests/testthat/test-read_payrolls.R
+++ tests/testthat/test-read_payrolls.R
@@ -9,7 +9,6 @@ test_that("read_payrolls() works", {
     expect_is(df$date, "Date")
     expect_is(df$value, "numeric")
     expect_gt(ncol(df), 4)
-    expect_gt(nrow(df), 1000)
     expect_false(any(grepl(" ", names(df))))
   }
 
